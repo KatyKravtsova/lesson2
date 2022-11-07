298875e288734b1db484d3d20a3bbf1118a335fd
Katy Kravtsova <kravtsovakaty@gmail.com>
2022-11-07 10:24:26 +0300

    1

---
+++ задачи/задача10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+char[] CharArray = input.ToCharArray();
+if(CharArray.Length > 3 | CharArray.Length < 3) Console.WriteLine("Введено не трехзначное число ");
+else Console.WriteLine(CharArray[1]);
+
